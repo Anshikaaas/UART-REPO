4848f3e9b5df3702e5543d39af5e8087d13b8751
Anshika <124487620+Anshikaaas@users.noreply.github.com>
2025-07-18 02:03:41 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,7 +1,3 @@
-/*
- * Simple testbench for UART.  Loop the rx and tx pins to each other and send
- * incrementing bytes, make sure that we receive what we expected.
- */
 `include "uart.v"
 
 module uart_tx_test();
@@ -17,42 +13,43 @@ wire [7:0] rxdata;
 wire loopback;
 reg rdy_clr = 0;
 
-uart test_uart(.din(data),
-	       .wr_en(enable),
-	       .clk_50m(clk),
-	       .tx(loopback),
-	       .tx_busy(tx_busy),
-	       .rx(loopback),
-	       .rdy(rdy),
-	       .rdy_clr(rdy_clr),
-	       .dout(rxdata));
+uart test_uart(
+    .din(data),
+    .wr_en(enable),
+    .clk_50m(clk),
+    .tx(loopback),
+    .tx_busy(tx_busy),
+    .rx(loopback),
+    .rdy(rdy),
+    .rdy_clr(rdy_clr),
+    .dout(rxdata)
+);
 
 initial begin
-	$dumpfile("uart.vcd");
-	$dumpvars(0, uart_tx_test);
-	enable <= 1'b1;
-	#2 enable <= 1'b0;
+    $dumpfile("uart.vcd");
+    $dumpvars(0, uart_tx_test);
+    enable <= 1'b1;
+    #2 enable <= 1'b0;
 end
 
-always begin
-	#1 clk = ~clk;
-end
+// 50 MHz clock (change #1 if you want a different speed)
+always #1 clk = ~clk;
 
 always @(posedge rdy) begin
-	#2 rdy_clr <= 1;
-	#2 rdy_clr <= 0;
-	if (rxdata != data) begin
-		$display("FAIL: rx data %x does not match tx %x", rxdata, data);
-		$finish;
-	end else begin
-		if (rxdata == 8'hff) begin
-			$display("SUCCESS: all bytes verified");
-			$finish;
-		end
-		data <= data + 1'b1;
-		enable <= 1'b1;
-		#2 enable <= 1'b0;
-	end
+    #2 rdy_clr <= 1;
+    #2 rdy_clr <= 0;
+    if (rxdata != data) begin
+        $display("FAIL: rx data %x does not match tx %x", rxdata, data);
+        $finish;
+    end else begin
+        if (rxdata == 8'hff) begin
+            $display("SUCCESS: all bytes verified");
+            $finish;
+        end
+        data <= data + 1'b1;
+        enable <= 1'b1;
+        #2 enable <= 1'b0;
+    end
 end
 
 endmodule
